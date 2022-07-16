44eacd8d3584949e11e13b5b84fc55ac1a1c9fdf
Angelina <angelinarwhite@gmail.com>
2022-07-15 23:48:07 -0400

    style projcts page

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -5,6 +5,7 @@ function AboutMe()
             <div id="aboutBackground"></div>
             <div id="aboutInfoBack"></div>
             <h1 className="header" id="aboutTitle">About Me</h1>
+            <h3 id="aboutDesc">About me information goes here...</h3>
         </div>
     )
 }
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,12 +1,38 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import construction from '../construction.jpg';
 
 function Projects()
 {
     return (
         <div id="projectsContainer">
-            <h1 className="header">Projects</h1>
+            <h1 className="header" id="projectTitle">Projects</h1>
+            <div id="projects">
+                <div id="proj1Back"></div>
+                <img src={ construction } className="constructionPhoto" />
+                <h2 className="projHeader">Job Application Tracker</h2>
+                <h3 className="projDes">Description:</h3>
+                <p className="projDescription">Description goes here...</p>
+                <h3 className="projLang">Languages:</h3>
+                <p className="projLanguages">Languages goes here...</p>
+                <a href="" className="projLinks">Demo</a>
+
+                <div id="proj2Back"></div>
+                <img src={ construction } className="constructionPhoto" />
+                <h2 className="projHeader">Shop We All</h2>
+                <h3 className="projDes">Description:</h3>
+                <p className="projDescription">Description goes here...</p>
+                <h3 className="projLang">Languages:</h3>
+                <p className="projLanguages">Languages goes here...</p>
+                <a href="" className="projLinks">Demo</a>
+
+                <div id="proj3Back"></div>
+                <img src={ construction } className="constructionPhoto" />
+                <h2 className="projHeader">Travel Planner</h2>
+                <h3 className="projDes">Description:</h3>
+                <p className="projDescription">Description goes here...</p>
+                <h3 className="projLang">Languages:</h3>
+                <p className="projLanguages">Languages goes here...</p>
+                <a href="" className="projLinks">Demo</a>
+            </div>
         </div>
     )
 }

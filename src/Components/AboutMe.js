45bdeea296d3142fd7d2576d989ef4c9d712773d
Angelina <angelinarwhite@gmail.com>
2022-07-17 14:21:16 -0400

    change font of about me page

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -8,7 +8,7 @@ function AboutMe()
             <h1 className="header" id="aboutTitle">About Me</h1>
             <div id="aboutTextCont">
                 <h3 id="aboutDescIntro">Hi! I'm Angelina.</h3>
-                <h3 id="aboutDesc">I've always had an interest in tech, minoring in Computer Science and working in the IT department at my school. I decided to take a leap and am now in the software engineering program at the coding bootcamp, Flatiron School. My last day is Aug. 5, and I am looking to start my new career as a software developer. While my skills are in both frontend and backend programming, my passion lies with frontend development, where I am able to work more creatively. </h3>
+                <h3 id="aboutDesc">I've always had an interest in tech, minoring in Computer Science and working in the IT department at my school. I decided to take a leap and am now in the software engineering program at the coding bootcamp, Flatiron School. My last day is Aug. 5, and I am currently looking for a position as a software developer. While my skills are in both frontend and backend programming, my passion lies with frontend development, where I am able to work more creatively. </h3>
             </div>
         </div>
     )

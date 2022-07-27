d4004cc79bc9bff007d3fe065e0e8072f2554770
Angelina <angelinarwhite@gmail.com>
2022-07-26 21:41:44 -0400

    cleanup

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,4 +1,3 @@
-import { AiOutlineArrowDown } from 'react-icons/ai';
 import homeImage2 from '../images/homeImage2.jpg';
 import { gsap } from "gsap";
 import { useEffect } from 'react';
@@ -8,12 +7,6 @@ function Home()
     useEffect(() =>
     {
         gsap.from("#homeImage", {duration: .5, opacity: 0});
-        // gsap.from("#nameBackground", {delay: .25, duration: 1.5, opacity: 0});
-        // gsap.from("#homeTitle", {delay: .5, duration: 1.5, opacity: 0});
-        // gsap.from("#homeTitle2", {delay: .55, duration: 1.5, opacity: 0});
-        // gsap.from("#homeArrow", {delay: .75, duration: 1, opacity: 0, y: -25});
-
-        // gsap.from("#one", {duration: .25, opacity: 0})
 
         //computer text typing
         var tl = gsap.timeline({delay: .25});
@@ -86,20 +79,8 @@ function Home()
     return (
         <div id="homeContainer">
             <img src={ homeImage2 } alt="homeImage" id="homeImage"/>
-            {/* <div id="homeElements">
-                <div id="nameBackground">
-                </div>
-                <div id="nameContainer">
-                    <h1 id="homeTitle">Angelina White</h1>
-                    <h2 id="homeTitle2">Software Engineer</h2>
-                </div> */}
-                {/* <p id="homeArrow" onClick={ handleClickArrow }><AiOutlineArrowDown /></p> */}
-            {/* </div> */}
 
             <div id="compWordsCont">
-                {/* <h1>{ title }</h1>
-                <h3>{ msg }</h3> */}
-
                 <div>
                     <h2 className="compWords brackets" id="a1">{ leftBrack }</h2>
                     <h2 className="compWords h" id="a2">h</h2>

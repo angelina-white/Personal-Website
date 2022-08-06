bd99912a33a7e833a046ee87d05894cf517b3a29
Angelina <angelinarwhite@gmail.com>
2022-08-06 11:51:34 -0400

    add more to about me

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -7,9 +7,9 @@ function AboutMe()
             <div id="aboutInfoBack"></div>
             <h1 className="header" id="aboutTitle">About Me</h1>
             <div id="aboutTextCont">
-                <h3 id="aboutDescIntro">Hi! I'm Angelina.</h3>
-                <h3 id="aboutDesc">I've always had an interest in tech, minoring in Computer Science and working in the IT department at my school. I just completed a coding bootcamp for software engineering and am searching for a new job as a full stack developer or front end developer.</h3>
-                <h3 id="aboutDesc2">All my jobs have been customer/client facing positions, which has allowed me to advance my communication skills, particularly in the technical field. I am able to articulate technical specifications to different types of people at different levels of understanding, which helps when working in diverse teams of varying roles and clients from different backgrounds.</h3>
+                <h4 id="aboutDescIntro">Hi! I'm Angelina.</h4>
+                <h4 id="aboutDesc">I've always had an interest in tech, minoring in computer science and working in the IT department at my school. I love learning new programming languages and skills to apply to my work. I’m currently learning React Native and taking the Google UX Design certification course. I just completed a coding bootcamp for software engineering and am searching for a new job as a full stack developer or front end developer.</h4>
+                <h4 id="aboutDesc2">All my jobs have been customer/client facing positions, which has allowed me to advance my communication skills, particularly in the technical field. I am able to articulate technical specifications to different types of people at different levels of understanding, which helps when working in diverse teams of varying roles and clients from multiple backgrounds.</h4>
             </div>
         </div>
     )

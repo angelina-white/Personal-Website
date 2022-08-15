e173701c09a33b62b397a596444d50dbb8060f99
Angelina <angelinarwhite@gmail.com>
2022-08-15 09:26:50 -0400

    change about me

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -8,7 +8,7 @@ function AboutMe()
             <h1 className="header" id="aboutTitle">About Me</h1>
             <div id="aboutTextCont">
                 <h4 id="aboutDescIntro">Hi! I'm Angelina.</h4>
-                <h4 id="aboutDesc">I've always had an interest in tech, minoring in computer science and working in the IT department at my school. I love learning new programming languages and skills to apply to my work. I’m currently learning React Native and Node.js. I just completed a coding bootcamp for software engineering and am searching for a new job as a full stack developer or front end developer.</h4>
+                <h4 id="aboutDesc">I've always had an interest in tech, minoring in computer science and working in the IT department at my school. I love learning new programming languages and skills to apply to my work. I’m currently learning React Native. I just completed a coding bootcamp for software engineering and am searching for a new job as a full stack developer or front end developer.</h4>
                 <h4 id="aboutDesc2">All my jobs have been customer/client facing positions, which has allowed me to advance my communication skills, particularly in the technical field. I am able to articulate technical specifications to different types of people at different levels of understanding, which helps when working in diverse teams of varying roles and clients from multiple backgrounds.</h4>
             </div>
         </div>
